203e1e39bc48f268ab47ba0939b4efc79e6058d3
“JazlynL” <lubenjazlyn35@gmail.com>
2022-02-24 19:02:07 -0500

    finish word count algorithm

---
+++ src/algorithimstiedtowordguesser/Main.java
@@ -0,0 +1,20 @@
+package algorithimstiedtowordguesser;
+
+public class Main {
+    public static void main(String [] args){
+        System.out.println(getCount("Celebration"));
+
+    }
+
+    public static int getCount(String str) {
+        int vowelsCount = 0;
+        // Your code here
+        for(int i = 0 ; i < str.length(); i++){
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e'|| str.charAt(i) == 'i'|| str.charAt(i) == 'o'||str.charAt(i)=='u'){
+                vowelsCount++;
+            }
+        }
+
+        return vowelsCount;
+    }
+}
